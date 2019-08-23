f1c00c068bae9d81d320ae3e034deaa619ec44fc
Angus McLeod <angus@mcleod.org.au>
2019-08-24 09:10:27 +1000

    Add category test

--- plugin.rb
+++ plugin.rb
@@ -6,5 +6,8 @@
 
 after_initialize do
   add_to_serializer(:site, :add_to_serializer_test_1) { "Test 1" }
-  add_to_serializer(:site, :add_to_serializer_test_2, false) { "Test 1" }
+  add_to_serializer(:site, :add_to_serializer_test_2, false) { "Test 2" }
+  
+  add_to_serializer(:basic_category, :add_to_serializer_test_1) { "Test 1" }
+  add_to_serializer(:basic_category, :add_to_serializer_test_2, false) { "Test 2" }
 end
