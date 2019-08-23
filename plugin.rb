0a44453c62ceef99ae4b1a6d5ba7a70b388e9283
Angus McLeod <angus@mcleod.org.au>
2019-08-24 08:55:23 +1000

    init

---
+++ plugin.rb
@@ -0,0 +1,10 @@
+# name: test-add-to-serializer
+# about: Testing add_to_serializer method
+# version: 1.0
+# authors: Angus McLeod
+# url: https://github.com/angusmcleod/test-add-to-serializer
+
+after_initialize do
+  add_to_serializer(:site, :add_to_serializer_test_1) { "Test 1" }
+  add_to_serializer(:site, :add_to_serializer_test_2, false) { "Test 1" }
+end
