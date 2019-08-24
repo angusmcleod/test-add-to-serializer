92a6ca772825deae76510ce8eff07beec99b2db6
Angus McLeod <angus@mcleod.org.au>
2019-08-24 11:08:00 +1000

    Add properties directly to the serializer

--- plugin.rb
+++ plugin.rb
@@ -11,8 +11,30 @@ after_initialize do
   add_to_serializer(:basic_category, :add_to_serializer_test_1) { "Basic Category Test 1" }
   add_to_serializer(:basic_category, :add_to_serializer_test_2, false) { "Basic Category Test 2" }
   
+  require_dependency 'basic_category_serializer'
+  class BasicCategorySerializer
+    def add_to_serializer_test_3
+      "Basic Category Test 3"
+    end
+    
+    def add_to_serializer_test_4
+      "Basic Category Test 4"
+    end
+  end
+  
   add_to_serializer(:site_category, :add_to_serializer_test_1) { "Site Category Test 1" }
   add_to_serializer(:site_category, :add_to_serializer_test_2, false) { "Site Category Test 2" }
+  
+  require_dependency 'site_category_serializer'
+  class SiteCategorySerializer
+    def add_to_serializer_test_3
+      "Site Category Test 3"
+    end
+    
+    def add_to_serializer_test_4
+      "Site Category Test 4"
+    end
+  end
   
   add_to_serializer(:topic_list_item, :add_to_serializer_test_1) { "Topic List Item Test 1" }
   add_to_serializer(:topic_list_item, :add_to_serializer_test_2, false) { "Topic List Item Test 2" }
