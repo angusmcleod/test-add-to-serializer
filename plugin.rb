3ce5165f78e62442c9ac0a32799909834390da38
Angus McLeod <angus@mcleod.org.au>
2019-08-24 10:55:42 +1000

    Add site_category and improve logging

--- plugin.rb
+++ plugin.rb
@@ -5,18 +5,21 @@
 # url: https://github.com/angusmcleod/test-add-to-serializer
 
 after_initialize do
-  add_to_serializer(:site, :add_to_serializer_test_1) { "Test 1" }
-  add_to_serializer(:site, :add_to_serializer_test_2, false) { "Test 2" }
+  add_to_serializer(:site, :add_to_serializer_test_1) { "Site Test 1" }
+  add_to_serializer(:site, :add_to_serializer_test_2, false) { "Site Test 2" }
   
-  add_to_serializer(:basic_category, :add_to_serializer_test_1) { "Test 1" }
-  add_to_serializer(:basic_category, :add_to_serializer_test_2, false) { "Test 2" }
+  add_to_serializer(:basic_category, :add_to_serializer_test_1) { "Basic Category Test 1" }
+  add_to_serializer(:basic_category, :add_to_serializer_test_2, false) { "Basic Category Test 2" }
   
-  add_to_serializer(:topic_list_item, :add_to_serializer_test_1) { "Test 1" }
-  add_to_serializer(:topic_list_item, :add_to_serializer_test_2, false) { "Test 2" }
+  add_to_serializer(:site_category, :add_to_serializer_test_1) { "Site Category Test 1" }
+  add_to_serializer(:site_category, :add_to_serializer_test_2, false) { "Site Category Test 2" }
   
-  add_to_serializer(:topic_view, :add_to_serializer_test_1) { "Test 1" }
-  add_to_serializer(:topic_view, :add_to_serializer_test_2, false) { "Test 2" }
+  add_to_serializer(:topic_list_item, :add_to_serializer_test_1) { "Topic List Item Test 1" }
+  add_to_serializer(:topic_list_item, :add_to_serializer_test_2, false) { "Topic List Item Test 2" }
   
-  add_to_serializer(:post, :add_to_serializer_test_1) { "Test 1" }
-  add_to_serializer(:post, :add_to_serializer_test_2, false) { "Test 2" }
+  add_to_serializer(:topic_view, :add_to_serializer_test_1) { "Topic View Test 1" }
+  add_to_serializer(:topic_view, :add_to_serializer_test_2, false) { "Topic View Test 2" }
+  
+  add_to_serializer(:post, :add_to_serializer_test_1) { "Post Test 1" }
+  add_to_serializer(:post, :add_to_serializer_test_2, false) { "Post Test 2" }
 end
